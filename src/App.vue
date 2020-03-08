256d064bc7053feb352299220517e01a07e67a3d
honey.rao <jsraoshihong@163.com>
2020-03-08 18:42:27 +0800

    单文件vue练习

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-    <div>这是登录组件</div>
+    <div>
+        <login></login>
+    </div>
 </template>
 <script>
+//引入Login.vue组件
+import login from './components/Login.vue'
+
 //使用export default将组件对象暴露出去
-export default {
-    
+export default {//不用指定el属性,自动绑定当前template上的根节点
+    components:{
+        "login":login
+    }
 }
 </script>
 <style scoped>
