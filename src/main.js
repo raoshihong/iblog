34966104405f72820a8d7e972911a15d3851a4dd
honey.rao <jsraoshihong@163.com>
2020-03-08 19:00:29 +0800

    export default

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-//引入vue模块,默认使用的是vue.runtime.common.js,要想使用vue.js,则需要在webpack.config.js中的resolve中配置
-import Vue from 'vue'
+import person,{birthday,dog} from "./test.js"
 
-//引入自定义的组件
-import App from './App.vue'
-
-// 将自定义组件渲染到容器中
-var vm = new Vue({
-    el:"#app",
-    render:(creatElement)=>creatElement(App)//直接通过render的方式渲染
-})
+console.log(person.name)
+console.log(person.age)
+console.log(birthday)
+console.log(dog)
---
+++ src/test.js
@@ -0,0 +1,12 @@
+//通过export default暴露对象
+export default {
+    name:"aa",
+    age:10
+}
+
+//通过export可以暴露多个对象
+export var birthday = "2012-10"
+export var dog = {
+    id : 10,
+    color:"red"
+}
